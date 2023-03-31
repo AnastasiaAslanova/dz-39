127d8d75c9df700d4042e7fa6098612a769c9b86
Anastasia Aslanova <nastatkachenko53@gmail.com>
2023-03-31 21:44:28 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+        <ul className="city-list">
+            <li className="city-list__item city-list__item--lisbon">
+                <h3 className="city-list__title">Lisbon</h3>
+            </li>
+            <li className="city-list__item city-list__item--paris">
+                <h3 className="city-list__title">Paris</h3>
+            </li>
+            <li className="city-list__item city-list__item--belgrade">
+                <h3 className="city-list__title">Belgrade</h3>
+            </li>
+
+            <li className="city-list__item city-list__item--venice">
+                <h3 className="city-list__title">Venice</h3>
+            </li>
+            <li className="city-list__item city-list__item--telaviv">
+                <h3 className="city-list__title">Tel-Avive</h3>
+            </li>
+            <li className="city-list__item city-list__item--cair">
+                <h3 className="city-list__title">Cair</h3>
+            </li>
+            <li className="city-list__item city-list__item--newyork">
+                <h3 className="city-list__title">New-York</h3>
+            </li>
+
+            <li className="city-list__item city-list__item--delhi">
+                <h3 className="city-list__title">New-Delhi</h3>
+            </li>
+            <li className="city-list__item city-list__item--francisco">
+                <h3 className="city-list__title">San-Francisco</h3>
+            </li>
+            <li className="city-list__item city-list__item--tokyo">
+                <h3 className="city-list__title">Tokyo</h3>
+            </li>
+
+            <li className="city-list__item city-list__item--sydney">
+                <h3 className="city-list__title">Sydney</h3>
+            </li>
+        </ul>
+
+    </div>
+  );
+}
+
+export default App;
